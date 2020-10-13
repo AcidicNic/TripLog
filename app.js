fe2b4accb99856a8f396f1a5ebf9412e494db990
Nicole Abadie <nic4096@gmail.com>
2020-10-13 13:58:17 -0700

    express project setup

---
+++ app.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const port = process.env.PORT || 80;
+
+const express = require('express');
+const hbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const path = require('path');
+
+const homeRouter = require('./routes/home.js');
+const logRouter = require('./routes/log.js');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
+
+app.engine('hbs', hbs({
+  extname: '.hbs',
+  layoutDir: __dirname + '/views',
+  partialsDir: __dirname + '/views/partials',
+  defaultLayout: 'base',
+}));
+app.set('view engine', 'hbs');
+
+app.use(express.static(path.normalize(path.join(__dirname, 'public'))));
+
+// Database Setup
+require('./data/db');
+
+// Routes
+app.use('/', homeRouter);
+app.use('/', logRouter);
+
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
+  });
+
+module.exports = app;
