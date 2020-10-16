b208ac51db2e92ca18947f7e53c3c0c157e9cf7c
Nicole Abadie <nic4096@gmail.com>
2020-10-16 14:34:42 -0700

    heorku

--- data/db.js
+++ data/db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
+require('dotenv').config();
 const assert = require("assert");
 
-const url = "mongodb://localhost/triplog-db";
+const url = process.env.MONGODB_URI || "mongodb://localhost/triplog-db";
 mongoose.Promise = global.Promise;
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.on("error", console.error.bind(console, "MongoDB connection Error:"));
